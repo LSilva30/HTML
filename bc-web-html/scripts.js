452397bb18de60511ab1cb01ad55fb3f52f0ff4a
luizsilva <luizfsilva.091595@gmail.com>
2021-07-20 15:38:51 -0400

    created a script folder to add my JS

---
+++ bc-web-html/scripts.js
@@ -0,0 +1,13 @@
+function handleSubmit(event) {
+  event.preventDefault()
+  console.log('Submitted Form')
+}
+function handleNameChange() {
+  console.log('Name Changed', event.target.value)
+}
+const hero = document.getElementById('hero')
+hero.innerText = "This is my new Hero Title"
+const links = document.getElementsByClassName('menu-link')
+links[1].style.color = "white"
+const footer = document.querySelector('footer')
+footer.style.backgroundColor = "grey"
